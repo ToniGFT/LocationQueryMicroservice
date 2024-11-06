9e0dab833d7ef438731cab2e647b40108c736559
Peral Belenguer <aipg@gft.com>
2024-11-06 10:13:04 +0100

    created the project

---
+++ src/main/java/com/gft/location_query_microservice/LocationQueryMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.gft.location_query_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationQueryMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationQueryMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gft/location_query_microservice/LocationQueryMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gft.location_query_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationQueryMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
