3b7ef4b0cd328cb61e956dad874c132c2b37be0a
Peral Belenguer <aipg@gft.com>
2024-11-27 10:41:44 +0100

    fixed an import

--- src/main/java/com/gft/location_query_microservice/infraestructure/messaging/kafka/LocationEventConsumer.java
+++ src/main/java/com/gft/location_query_microservice/infraestructure/messaging/kafka/LocationEventConsumer.java
@@ -3,6 +3,7 @@ package com.gft.location_query_microservice.infraestructure.messaging.kafka;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gft.location_query_microservice.infraestructure.messaging.kafka.strategies.EventProcessorRegistry;
 import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
